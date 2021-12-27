ed18e784422c75c3b386af9fe7d48a91c17d409f
cf <hoge>
2021-12-27 13:01:54 +0900

    コレクションの相互変換

---
+++ day3/c2_23.py
@@ -0,0 +1,10 @@
+scores = {'network':60, 'database':80, 'security':60}
+members = ['松田','浅木','工藤']
+print(tuple(members))#リストmembersをタプルに変換
+print(list(scores))#scoresのキーをリストに変換して表示
+print(set(scores.values()))#scoresの値をセットに変換
+
+color_en=['red','green','blue']
+color_ja=['赤','緑','青']
+color=dict(zip(color_en.color_ja))
+print(color)
