9d8c04026c82feec32178d43ac17634d44e0f7d5
cf <hoge>
2021-12-28 11:43:05 +0900

    ex2_2変更 JSだった場合の処理追加

--- day4/ex2_2.py
+++ day4/ex2_2.py
@@ -4,4 +4,12 @@ scores.append(int(input('算数の点数>>')))
 scores.append(int(input('理科の点数>>')))
 scores.append(int(input('社会の点数>>')))
 scores.append(int(input('英語の点数>>')))
+#let li=[]; js
+#li.push(10); js
+#li.push(20); js
+#let sum=0;
+#for(let i=0; i<li.length; i++){
+#   sum+=li[i];
+#  }
+#console.log('合計点${s},平均点${avg}')  jsでの書き方　テスト出る？
 print(f'合計{sum(scores)}点/平均{sum(scores)/len(scores)}点')
