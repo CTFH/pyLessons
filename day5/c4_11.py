d6ffe87d76d342a58e9e7d58f657ff2c834f71d2
cf <hoge>
2022-01-06 13:22:04 +0900

    c4_11追加 continueでループを１回スキップすることができる isinstance(対象, 型)で対象がその型なのかを判定できる

---
+++ day5/c4_11.py
@@ -0,0 +1,13 @@
+ages=[28,50,'ひみつ',20,78,25,22,10,'無回答',33]
+samples=list()
+for data in ages:
+    #if (! data instanceof Integer){}
+    if not isinstance(data, int): #整数でないデータはスキップ
+        #isinstance(データ、データ型）
+        #
+        continue
+    if data < 20 or data >= 30:
+        continue
+    
+    samples.append(data)
+print(samples)
