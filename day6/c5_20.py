0a7a1f4a72bf8dc4675f7198cd6a6cd8bf8fabee
cf <hoge>
2022-01-07 13:17:38 +0900

    c5_22,c5_24,c5_29追加 デフォルト引数、キーワード引数、可変長引数

---
+++ day6/c5_20.py
@@ -0,0 +1,9 @@
+def eat(breakfast, lunch, dinner):
+    print('朝は{}を食べました'.format(breakfast))
+    print('昼は{}を食べました'.format(lunch))
+    print('晩は{}を食べました'.format(dinner))
+
+eat('トースト','おにぎり','カレー')
+eat('納豆ご飯','ラーメン','カレー')
+eat('バナナ','そば','焼肉')
+eat('サンドウィッチ','しゅうまい弁当','カレー')
