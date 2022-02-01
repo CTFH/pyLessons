197e6dbeff00e5934ba2bb18d3534cde48e4ef63
cf <hoge>
2022-02-01 15:16:47 +0900

    修正

--- HWpy/testEx2.py
+++ HWpy/testEx2.py
@@ -1,9 +1,25 @@
 import random
+import string
+
 members=['大迫','古橋','久保','堂安','南野','伊東','田中','遠藤','富安','吉田','長友','酒井']
 
 random.shuffle(members)
 
+#def listAlphabet():
+#    return list(string.ascii_uppercase)
+
+
 teamNum=int(input('いくつチームを作りますか(1-12)?'))
+
+alph=[A,B,C,D,E,F,G,H,I,J,K,L]
+for i in range teamNum:
+    list(alph[i]) = []
+
+#teamPpl=len(members)//teamNum
+#teamPplEx=len(members)%teamNum
+
+for i in range (len(members)):
+    list(alph[i])=members[i]
 
 length=int(len(members)/teamNum)
 #if len(members)%teamNum!=0:
